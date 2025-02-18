e25b0ec2481a52efab0e85e4715ffd30bf7db5f3
Renning Bruns <ren@renmail.net>
2025-02-18 14:35:01 -0800

    create trait to use for server message handler

--- crates/email-server-core/src/smtp/message.rs
+++ crates/email-server-core/src/smtp/message.rs
@@ -1,3 +1,5 @@
+use std::error::Error;
+use async_trait::async_trait;
 use derive_builder::Builder;
 
 #[derive(Default, Builder, Debug, Clone)]
@@ -6,4 +8,22 @@ pub struct Message {
     pub from: String,
     pub to: Vec<String>,
     pub data: Vec<u8>,
+}
+
+#[async_trait]
+pub trait Handler {
+    async fn handle_message(&self, message: Message) -> Result<(), Box<dyn Error>>;
+}
+
+pub struct PrintHandler;
+
+#[async_trait]
+impl Handler for PrintHandler {
+    async fn handle_message(&self, message: Message) -> Result<(), Box<dyn Error>> {
+        println!("Received message from {} to {} with {} bytes of data",
+                 message.from,
+                 message.to.join(", "),
+                 message.data.len());
+        Ok(())
+    }
 }
