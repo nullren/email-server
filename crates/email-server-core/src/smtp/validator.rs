e7169f81f58ca169406651941965fc2b0e7dfd4f
Renning Bruns <ren@renmail.net>
2025-02-25 15:06:13 -0800

    validator starting...

---
+++ crates/email-server-core/src/smtp/validator.rs
@@ -0,0 +1,33 @@
+use async_trait::async_trait;
+use trust_dns_resolver::{
+    config::{ResolverConfig, ResolverOpts},
+    TokioAsyncResolver,
+};
+
+#[async_trait]
+pub trait HeloValidator {
+    async fn valid(&self, _domain: &str) -> bool {
+        true
+    }
+}
+
+#[derive(Debug)]
+pub struct NoopValidator;
+impl HeloValidator for NoopValidator {}
+
+#[derive(Debug)]
+pub struct DomainNameValidator;
+#[async_trait]
+impl HeloValidator for DomainNameValidator {
+    async fn valid(&self, domain: &str) -> bool {
+        let resolver =
+            TokioAsyncResolver::tokio(ResolverConfig::default(), ResolverOpts::default());
+        // valid domain
+        if let Ok(response) = resolver.lookup_ip(domain).await {
+            if response.iter().next().is_some() {
+                return true;
+            }
+        }
+        false
+    }
+}
